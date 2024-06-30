3db8f55f9facdc35b34a691670efc345f8d4bb97
Shrish <shrish.dollin1@gmail.com>
2024-06-30 20:10:35 +0530

    basic tasks using recursion

--- Recursion/one.java
+++ Recursion/one.java
@@ -1,16 +1,43 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class one {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+        // Scanner scn = new Scanner(System.in);
+        // int n = scn.nextInt();
 
         // int ans = fact(n);
         // System.out.println(ans);
 
         // nth fibonacci number
-        int ans = fibN(n);
+        // int ans = fibN(n);
+        // System.out.println(ans);
+
+        // power function x to power n
+        // int ans = pow(5, 3);
+        // System.out.println(ans);
+
+        // print n numbers
+        // printNumbers(10);
+
+        // count of digits
+        int ans = counOfDigits(2833382);
         System.out.println(ans);
+    }
+
+    private static int counOfDigits(int n) {
+        if(n == 0) return 0;
+        return counOfDigits(n/10) + 1;
+    }
+
+    private static void printNumbers(int n) {
+        if(n == 0) return;
+        printNumbers(n - 1);
+        System.out.print(n + " ");
+    }
+
+    private static int pow(int x, int n) {
+        if(n == 0) return 1;
+        return x * pow(x, n - 1);
     }
 
     private static int fibN(int n) {
