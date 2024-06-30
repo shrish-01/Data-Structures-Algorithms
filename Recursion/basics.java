8de0502f0565aba62f6324978fbcd7b94bb4b26c
Shrish <shrish.dollin1@gmail.com>
2024-06-30 22:46:05 +0530

    refactoring file name from one.java to basics.java

--- Recursion/basics.java
+++ Recursion/basics.java
@@ -1,6 +1,6 @@
 // import java.util.Scanner;
 
-public class one {
+public class basics {
     // public static long ans;
     public static void main(String[] args) {
         // Scanner scn = new Scanner(System.in);
