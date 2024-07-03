89751f029b24dbb6d16c31b7a8a3de2d4272536e
Shrish <shrish.dollin1@gmail.com>
2024-07-03 18:51:02 +0530

    first commit from intellij-added sum of array-recursion

--- Recursion/intermediate.java
+++ Recursion/intermediate.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class intermediate {
     public static void main(String[] args) {
         // check if array is sorted
+//        int arr[] = {1, 2, 7, 4, 5};
+//        int n = arr.length;
+//        boolean ans = isSorted(arr, 0, n - 1, n);
+//        System.out.println(ans);
+
+        // sum of array:
         int arr[] = {1, 2, 7, 4, 5};
-        int n = arr.length;
-        boolean ans = isSorted(arr, 0, n - 1, n);
+        int ans = sumOfArray(arr, 0);
         System.out.println(ans);
+    }
+
+    private static int sumOfArray(int[] arr, int i) {
+        if(i == arr.length) {
+            return 0;
+        }
+        return sumOfArray(arr, i + 1) + arr[i];
     }
 
     private static boolean isSorted(int[] arr, int i, int j, int n) {
