afae1a03a15ffa3b347dbfc71e7e00f51c912551
Shrish <shrish.dollin1@gmail.com>
2024-08-03 00:37:39 +0530

    update

--- Generic Trees/src/Main.java
+++ Generic Trees/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 class GenericTree<T> {
     T data;
@@ -12,6 +10,8 @@ public class Main {
         ArrayList<Integer> treeInput = new ArrayList<>(Arrays.asList(10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1));
         GenericTree<Integer> root = constructGenericTree(treeInput);
         display(root);
+        int ans = sizeOfGenericTree(root);
+        System.out.println(ans);
     }
 
     public static GenericTree<Integer> constructGenericTree(ArrayList<Integer> treeInput) {
@@ -49,5 +49,32 @@ public class Main {
 //            System.out.println(child.data);
             display(child);
         }
+    }
+
+    public static int sizeOfGenericTree(GenericTree<Integer> root) {
+//        Queue<GenericTree<Integer>> q = new LinkedList<>();
+//        q.offer(root);
+//        int size = 0;
+//
+//        while(!q.isEmpty()) {
+//            GenericTree<Integer> node = q.peek();
+//            size++;
+//            for(GenericTree<Integer> n: node.children) {
+//                q.offer(n);
+//            }
+//            q.poll();
+//        }
+//
+//        return size;
+
+        // recursive solution
+        int size = 0;
+        for(GenericTree<Integer> node : root.children) {
+            int sizeOfSubtree = sizeOfGenericTree(node);
+            size += sizeOfSubtree;
+        }
+
+        size += 1;
+        return size;
     }
 }
